26fe33d4316554755cfbae6254229a62d8382bbd
Stefano Baghino <stefano@baghino.me>
2023-03-04 11:45:52 +0100

    chore: pin nixpkgs dependency, allow usage of nix-managed tooling

--- shell.nix
+++ shell.nix
@@ -1,10 +1,7 @@
-with import <nixpkgs> {};
-
-mkShell {
-  nativeBuildInputs = [
-    rustc cargo libiconv
-  ];
-
-  # Set Environment Variables
+let
+  pkgs = import (fetchTarball("https://github.com/NixOS/nixpkgs/archive/22.11.tar.gz")) {};
+in pkgs.mkShell {
+  buildInputs = [ pkgs.cargo pkgs.rustc pkgs.libiconv ];
+  RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
   RUST_BACKTRACE = 1;
 }
